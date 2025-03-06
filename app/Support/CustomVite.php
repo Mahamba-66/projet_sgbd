08bced7276b0f6d424984857fd68fe3ade083c33
Moct-art <148657292+Moct-art@users.noreply.github.com>
2025-03-06 02:59:01 +0000

    modification custom

--- app/Support/CustomVite.php
+++ app/Support/CustomVite.php
@@ -9,7 +9,6 @@ class CustomVite extends BaseVite
 {
     protected function chunk($manifest, $file)
     {
-        // Si le fichier est app.css, on le remplace par le bon chemin
         if ($file === 'resources/css/app.css') {
             return [
                 'file' => 'assets/app-DM65pi2Y.css',
@@ -23,9 +22,7 @@ class CustomVite extends BaseVite
 
     public function __invoke($entrypoints = '')
     {
-        $entrypoints = is_string($entrypoints)
-            ? [$entrypoints]
-            : $entrypoints;
+        $entrypoints = is_string($entrypoints) ? [$entrypoints] : $entrypoints;
 
         $this->withEntryPoints($entrypoints);
 
